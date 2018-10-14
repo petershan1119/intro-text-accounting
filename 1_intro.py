f7aa452120a285be12b6a0c3c30d1a10d5e14b95
Sang Won Han <petershan0315@gmail.com>
2018-10-14 12:43:36 +0900

    basic functions

---
+++ 1_intro.py
@@ -0,0 +1,98 @@
+toy_corpus = ["The fox jumps over the dog", "The fox is very clever and quick",
+              "The dog is slow and lazy", "The cat is smarter than the fox and the dog",
+              "Python is an excellent programming language", "Java and Ruby are other programming language",
+              "Python and Java are very popular programming language", "Python programs are smaller than Java programs"]
+
+from gensim import corpora, models
+
+import numpy as np
+from html.parser import HTMLParser
+import re
+from contractions import CONTRACTION_MAP
+import nltk
+import string
+
+from nltk.corpus import wordnet as wn
+from pattern.en import tag
+from nltk.stem import WordNetLemmatizer
+
+wnl = WordNetLemmatizer
+
+def pos_tag_text(text):
+    def penn_to_wn_tags(pos_tag):
+        if pos_tag.startswith('J'):
+            return wn.ADJ
+        elif pos_tag.startswith('V'):
+            return wn.VERB
+        elif pos_tag.startswith('N'):
+            return wn.NOUN
+        elif pos_tag.startswith('R'):
+            return wn.ADV
+        else:
+            return None
+
+    tagged_text = tag(text)
+    tagged_lower_text = [(word.lower(), penn_to_wn_tags(pos_tag)) for word, pos_tag in tagged_text]
+    return tagged_lower_text
+
+
+def lemmatize_text(text):
+    pos_tagged_text = pos_tag_text(text)
+    lemmatized_tokens = [wnl.lemmatize(word, pos_tag) if pos_tag else word for word, pos_tag in pos_tagged_text]
+    lemmatized_text = ' '.join(lemmatized_tokens)
+    return lemmatized_text
+
+
+html_parser = HTMLParser()
+
+def tokenize_text(text):
+    tokens = nltk.word_tokenize(text)
+    tokens = [token.strip for token in tokens]
+    return tokens
+
+def expand_contractions(text, contraction_mapping):
+    contractions_mapping = re.compile('({})'.format('|'.join(contraction_mapping.keys())), flags=re.IGNORECASE|re.DOTALL)
+
+    def expand_match(contraction):
+        match = contraction.group(0)
+        first_char = match[0]
+        expanded_contraction = contraction_mapping.get(match) if contraction_mapping.get(match) else contraction_mapping.get(match.lower())
+        expanded_contraction = first_char + expanded_contraction[1:]
+        return expanded_contraction
+
+
+stopword_list = nltk.corpus.stopwords.words('english')
+
+
+def remove_stopwords(text):
+    tokens = tokenize_text(text)
+    filtered_tokens = [token for token in tokens if token not in stopword_list]
+    filtered_text = ' '.join(filtered_tokens)
+    return filtered_text
+
+
+def remove_special_character(text):
+    tokens = tokenize_text(text)
+    pattern = re.compile('[{}]'.format(re.escape(string.punctuation)))
+    filtered_tokens = filter(None, [pattern.sub('', token) for token in tokens])
+    filtered_text = ' '.join(filtered_tokens)
+    return filtered_text
+
+
+def normalize_corpus(corpus, lemmatize=True, tokenize=True):
+    normalized_corpus = []
+    for text in corpus:
+        # text = html_parser.unescape(text)
+        text = expand_contractions(text, CONTRACTION_MAP)
+        if lemmatize:
+            text = lemmatize_text(text)
+        else:
+            text = text.lower()
+        text = remove_special_character(text)
+        text = remove_stopwords(text)
+        if tokenize:
+            text = tokenize_text(text)
+            normalized_corpus.append(text)
+        else:
+            normalized_corpus.append(text)
+    return normalized_corpus
